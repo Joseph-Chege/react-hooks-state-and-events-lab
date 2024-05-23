c00db9c75964216546a899f60c04ad4cac0db77a
Joseph-Chege <mjchege23@gmail.com>
2024-05-23 21:59:26 +0300

    Updated state based on events and worked with multiple state variables together

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react";
 
 function ShoppingList({ items }) {
+
+  const [foods, setFoods] = useState(items);
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  const foodsToDisplay = foods.filter((food) => {
+    if(selectedCategory === "All") {
+      return true;
+    }else {
+      return food.category === selectedCategory;
+    }
+  })
+
+  function handleFilterChange (e) {
+    setSelectedCategory(e.target.value)
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilterChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +30,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {foodsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
